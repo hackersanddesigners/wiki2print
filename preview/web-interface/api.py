575d31c00d390f8139d0fc4746013f05f2947576
كارل مبارك <bonjour@moubarak.eu>
2022-02-16 15:16:01 +0100

    testig zulip-github webhook

--- preview/web-interface/api.py
+++ preview/web-interface/api.py
@@ -11,7 +11,7 @@ STATIC_FOLDER_PATH = './static' # without trailing slash
 PUBLIC_STATIC_FOLDER_PATH = '/static' # without trailing slash
 TEMPLATES_DIR = None
 
-# does git work
+# does the zulip=github webhook work?
 
 # do API request and return JSON
 def do_API_request(url):
